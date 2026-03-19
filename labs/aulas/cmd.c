41bb13cb12cbe8b21326a76e5748e81de75234b2
eugenio-labgrad <eanteloa@proton.me>
2026-03-19 10:32:13 -0300

    aula 19-03

---
+++ labs/aulas/cmd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    //Quantidade de argumentos
+    printf("Quantidade de argumentos: argc = %d\n,", argc);
+    
+    //Valor (quais sãp) dos argumentos
+    for(int i =0; i<arg; i++)(
+        
+    )
+    return 0;
+} 
